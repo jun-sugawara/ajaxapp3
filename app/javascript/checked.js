90c82703b40997b71d38015a70d881545ee472a3
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-08-09 10:09:16 +0900

    リクエストの詳細を設定

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -1,7 +1,11 @@
 function check() {
   const posts = document.querySelectorAll(".post");
   posts.forEach(function (post){
-    post.addEventListener("click", ()=> {});
+    post.addEventListener("click", ()=> {
+      const postId = post.getAttribute("data-id");
+      const XHR = new XMLHttpRequest();
+      XHR.open("GET", `/posts/${postId}`, true);
+    });
   });
 }
 window.addEventListener("load", check);
