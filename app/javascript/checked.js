e2f19fdc0042c8c30a90ea8f2e13b05453f62bdc
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-08-10 10:14:27 +0900

    タイプミスを修正

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -1,12 +1,12 @@
 function check() {
   const posts = document.querySelectorAll(".post");
-  posts.forEach(function (post){
-    post.addEventListener("click", ()=> {
+  posts.forEach(function (post) {
+    post.addEventListener("click", () => {
       const postId = post.getAttribute("data-id");
       const XHR = new XMLHttpRequest();
       XHR.open("GET", `/posts/${postId}`, true);
       XHR.responseType = "json";
-      XMLHttpRequest.send();
+      XHR.send();
     });
   });
 }
