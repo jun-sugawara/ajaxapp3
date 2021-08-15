52ce0d0a7c5d0cf806fff0bcb635c08d581d4d40
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-08-15 11:12:19 +0900

    リクエストの送信

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,9 +1,11 @@
 function memo() {
   const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
+    const formData = new FormData(document.getElementById("form"));
     const XHR = new XMLHttpRequest();
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
+    XHR.send(formData);
   });
 }
-window.addEventListener("Load", memo);
+window.addEventListener("load", memo);
